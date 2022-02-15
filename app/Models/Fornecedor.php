25262654ae1ac82d0a35bc3f5150e3090b6e54d9
DiogoHaro <diogo-haro@unidavi.edu.br>
2022-02-14 22:10:41 -0300

    Aula Eloquent

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    // Corrige o pradrão de nomeação de tabelas do ELOQUENT
+    protected $table = 'fornecedores';
+    protected $fillable = ['nome', 'site', 'email', 'uf', 'email'];
 }
